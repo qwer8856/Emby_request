93f930d79ef287dacc3924e1c0472e9389cd6f82
qwer8856 <472361136@qq.com>
2026-04-17 16:17:01 +0800

    bump version to 2.1.2

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 import hashlib
 
 # 应用版本号
-APP_VERSION = '2.1.1'
+APP_VERSION = '2.1.2'
 import time
 import threading
 from threading import Lock, Thread, Event
