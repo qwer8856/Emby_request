df02ad6af438c6714d82e4b46f0ca5709c752f7b
qwer8856 <472361136@qq.com>
2026-04-20 15:16:58 +0800

    chore: release v2.2.58 with profile layout adjustments

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 import hashlib
 
 # 应用版本号
-APP_VERSION = '2.2.57'
+APP_VERSION = '2.2.58'
 import time
 import threading
 from threading import Lock, Thread, Event
